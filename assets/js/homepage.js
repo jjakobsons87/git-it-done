e34eb2c4deb35b2f3ef2c61461b01f65578e30c0
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-10 00:00:27 -0500

    updated call to be generic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
-var getUserRepos = function() {
-    var response = fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+var getUserRepos = function(user) {
+    // format the github api url 
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a reqeust to the URL 
+    fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
