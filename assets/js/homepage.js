4b235d1750b382dc74e562e479f1c3be2d044c0b
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-10 00:15:24 -0500

    added search box and true call

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,6 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
 var getUserRepos = function(user) {
     // format the github api url 
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -10,4 +13,17 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos();
+function formSubmitHandler(event) {
+    event.preventDefault();
+    // get value from input element 
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please Enter a GitHub username")
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
