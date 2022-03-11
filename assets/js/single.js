79257e02efc9aaa2d4d7c8107cd6443c4feb00c1
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-03-10 21:39:40 -0500

    final logic

--- assets/js/single.js
+++ assets/js/single.js
@@ -4,12 +4,21 @@ var repoNameEl = document.querySelector("#repo-name");
 
 
 function getRepoName(){
+    // grab repo name from url query string 
     var queryString = document.location.search;
     var repoName = queryString.split("=")[1];
-    getRepoIssues(repoName);
-    repoNameEl.textContent = repoName;
-    // console.log(repoName);
-}
+
+    if (repoName) {
+        // display repo name on the page 
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage 
+        document.location.replace("./index.html");
+    }
+};
+
+
 
 // get the repo
 function getRepoIssues(repo) {
@@ -27,9 +36,10 @@ function getRepoIssues(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to homepage 
+            document.location.replace("./index.html");
         }
-        getRepoName();
+        // getRepoName();
     });
     
 };
